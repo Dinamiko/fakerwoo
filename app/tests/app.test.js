cbd9645e2aeec7a8521738205aee6e359b13e5b9
Emili <emili.castells@dinamiko.com>
2018-09-20 19:09:23 +0200

    Add enzyme

--- app/tests/app.test.js
+++ app/tests/app.test.js
@@ -1,3 +1,8 @@
-test('sum', () => {
-    expect(1+1).toEqual(2);
+import React from 'react';
+import {shallow} from 'enzyme';
+import App from '../App';
+
+test('shows app title', () => {
+   const container = shallow(<App/>);
+   expect(container.find('h1').length).toEqual(1);
 });
